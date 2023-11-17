fd8af84e31976872c41dcd2fd4901a2e64a79109
DHARANIKA R <124855076+DHARANIKARAJU@users.noreply.github.com>
2023-11-17 11:47:15 +0530

    Day4

---
+++ Day4/reactapp3/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import EmployeeDetails from './components/List';
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeDetails/>
+    </div>
+  );
+}
+
+export default App;
